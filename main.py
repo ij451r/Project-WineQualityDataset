4e5f37c00e1c71227c03058fb7d6f5e7a93f76aa
unknown <mohammedjasir660@gmail.com>
2023-09-29 23:26:16 +0400

    data-validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from WineQuality import logger
 from WineQuality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from WineQuality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -7,6 +8,16 @@ try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     data_ingestion_training_pipeline = DataIngestionTrainingPipeline()
     data_ingestion_training_pipeline.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+except Exception as e:
+    logger.exception(e)
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_validation_training_pipeline = DataValidationTrainingPipeline()
+    data_validation_training_pipeline.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
 except Exception as e:
     logger.exception(e)
---
+++ src/WineQuality/pipeline/stage_02_data_validation.py
@@ -0,0 +1,24 @@
+from WineQuality.config.configuration import ConfigurationManager
+from WineQuality.components.data_validation import DataValidation 
+from WineQuality import logger
+
+STAGE_NAME = "Data Validation Stage"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        data_validation_training_pipeline = DataValidationTrainingPipeline()
+        data_validation_training_pipeline.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+    except Exception as e:
+        logger.exception(e)
