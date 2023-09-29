4a6f4c2b503a5b1d76d478b270fb74aaa0d3e636
unknown <mohammedjasir660@gmail.com>
2023-09-29 21:24:44 +0400

    data-ingestion

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from WineQuality import logger
+from WineQuality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("HelloWorld")  
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion_training_pipeline = DataIngestionTrainingPipeline()
+    data_ingestion_training_pipeline.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+except Exception as e:
+    logger.exception(e)
